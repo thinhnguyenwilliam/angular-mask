68293a4779993df833f64a81221e6ff7ad04a144
ThinhNguyen <thinhproee@gmail.com>
2025-05-18 20:42:45 +0700

    angular mask

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { Section1Component } from './components/section1/section1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    Section1Component
   ],
   imports: [
     BrowserModule
